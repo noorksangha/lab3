3175efb690827cfbf5a26fa52f7af3bf732f7b2d
noorksangha <113377440+noorksangha@users.noreply.github.com>
2024-02-04 14:45:49 -0800

    Update server.js

--- lab3/api/server.js
+++ lab3/api/server.js
@@ -2,15 +2,13 @@ const http = require('http');
 const url = require('url');
 const utils = require('./modules/util');
 
-http.createServer(function(request,response){
+module.exports = (request, response) => {
     const queryObject = url.parse(request.url, true).query;
 
     const name = queryObject.name || 'Guest';
     const text = queryObject.text || 'Fake';
     const currentTime = utils.getDate();
 
-    response.writeHead(200,{'Content-Type':'text/html'});
+    response.writeHead(200, {'Content-Type': 'text/html'});
     response.end(`<p style="color: blue;">Hello ${name}, What a beautiful day. Server current date and time is ${currentTime}</p>`);
-
-}).listen(8080);
-console.log('Server is running on port 8080')
+};
